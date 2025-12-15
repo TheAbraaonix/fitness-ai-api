de43f46d17f701fc6adb3a31aadf55f17cdf8fa1
TheAbraaonix <carlosabraao@hotmail.com.br>
2025-12-15 18:33:01 -0300

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/carlosholanda/fitness_ai_api/FitnessAiApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlosholanda.fitness_ai_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosholanda/fitness_ai_api/FitnessAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosholanda.fitness_ai_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
